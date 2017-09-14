2d98b2cb29c0bc2a8e39f24f0ec813c28a698bd2
marcelooliveira <mclricardo@gmail.com>
2017-09-14 18:15:26 -0300

    Aula 5
    Aula 5

---
+++ A5.1-Covariancia/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5._1_Covariancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("string para object");
+            string titulo = "meses";
+            object obj = titulo;
+            Console.WriteLine(obj);
+            ///<image url="$(ProjectDir)\Slides\img1.png" scale=""/>
+
+            Console.WriteLine("List<string> para List<object>");
+
+            IList<string> listaMeses = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+            //IList<object> listaObj = listaMeses;
+            Console.WriteLine();
+
+            Console.WriteLine("string[] para object[]?");
+            string[] arrayMeses = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+            object[] arrayObj = arrayMeses; //COVARIÂNCIA!
+            Console.WriteLine(arrayObj);
+            foreach (var item in arrayObj)
+            {
+                Console.WriteLine(item);
+            }
+            ///<image url="$(ProjectDir)\Slides\img3.png" scale=""/>
+            arrayObj[0] = "PRIMEIRO MÊS";
+            Console.WriteLine(arrayObj[0]);
+            Console.WriteLine();
+
+            //arrayObj[0] = 12345;
+            //Console.WriteLine(arrayObj[0]);
+            //Console.WriteLine();
+
+            Console.WriteLine("List<string> para IEnumerable<object>");
+
+            IEnumerable<object> enumObj = listaMeses; //COVARIÂNCIA
+            foreach (var item in enumObj)
+            {
+                Console.WriteLine(item);
+            }
+            //enumObj[0] = 12345;
+        }
+    }
+}
---
+++ A5.2-ModificandoAoEnumerar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5._2_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meses = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            foreach (var mes in meses)
+            {
+                meses[0] = meses[0].ToUpper();
+                Console.WriteLine(mes);
+            }
+
+            //for (int i = 0; i < meses.Length; i++)
+            //{
+            //    var mes = meses[i];
+            //    ....
+            //}
+        }
+    }
+}
