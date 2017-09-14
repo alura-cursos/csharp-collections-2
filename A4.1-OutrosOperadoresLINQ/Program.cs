2dd14403e95d9e852be727e5e43a4c50f294d217
marcelooliveira <mclricardo@gmail.com>
2017-09-14 18:12:34 -0300

    Aula 4
    Aula 4

---
+++ A4.1-OutrosOperadoresLINQ/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4._1_OutrosOperadoresLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mes> meses = new List<Mes>
+            {
+               new Mes("Janeiro   ",31),
+               new Mes("Fevereiro ",28),
+               new Mes("Março     ",31),
+               new Mes("Abril     ",30),
+               new Mes("Maio      ",31),
+               new Mes("Junho     ",30),
+               new Mes("Julho     ",31),
+               new Mes("Agosto    ",31),
+               new Mes("Setembro  ",30),
+               new Mes("Outubro   ",31),
+               new Mes("Novembro  ",30),
+               new Mes("Dezembro  ",31)
+            };
+
+            //Pegar o primeiro trimestre
+            var consulta = meses.Take(3);
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //Pegar os meses depois do primeiro trimestre
+            var consulta2 = meses.Skip(3);
+            foreach (var item in consulta2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //Pegar os meses do terceiro trimestre
+            var consulta3 = meses.Skip(6).Take(3);
+            foreach (var item in consulta3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //Pegar os meses até que o mês comece com a letra 'S'
+            var consulta4 = meses.TakeWhile(m => !m.Nome.StartsWith("S"));
+            foreach (var item in consulta4)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //Pular os meses até que o mês comece com a letra 'S'
+            var consulta5 = meses.SkipWhile(m => !m.Nome.StartsWith("S"));
+            foreach (var item in consulta5)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Mes : IComparable
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        public int CompareTo(object obj)
+        {
+            Mes outro = obj as Mes;
+
+            return this.Nome.CompareTo(outro.Nome);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Dias}";
+        }
+    }
+}
---
+++ A4.2-OperadoresDeConjuntos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4._2_OperadoresDeConjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] seq1 = { "janeiro", "fevereiro", "março" };
+            string[] seq2 = { "fevereiro", "MARÇO", "abril" };
+
+            Console.WriteLine("Concatenando duas sequências");
+            ///<image url="$(ProjectDir)\Slides\img1.png" scale=""/>
+            var consulta = seq1.Concat(seq2);
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("União de duas sequências");
+            ///<image url="$(ProjectDir)\Slides\img2.png" scale=""/>
+            var consulta2 = seq1.Union(seq2);
+            foreach (var item in consulta2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("União de duas sequências com comparador");
+            var consulta3 = seq1.Union(seq2, StringComparer.InvariantCultureIgnoreCase);
+            foreach (var item in consulta3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Interseção de duas sequências");
+            ///<image url="$(ProjectDir)\Slides\img4.png" scale=""/>
+            var consulta4 = seq1.Intersect(seq2);
+            foreach (var item in consulta4)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Exceto: elementos de seq1 que não estão em seq2");
+            ///<image url="$(ProjectDir)\Slides\img5.png" scale=""/>
+            var consulta5 = seq1.Except(seq2);
+            foreach (var item in consulta5)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
